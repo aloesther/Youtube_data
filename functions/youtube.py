629cabb3c79963b005e359603de14c535671b1c9
Shyn Tech <professorbashorun@gmail.com>
2022-11-18 05:58:51 +0100

    updates

--- functions/youtube.py
+++ functions/youtube.py
@@ -1,9 +1,39 @@
-from .utils import *;
-from pyyoutube import Api
+#from .utils import *;
+import os
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
 
 
 
-ytapi = Api(api_key="your api key")
+
+scopes = ['https://www.googleapis.com/auth/youtube.readonly','https://www.googleapis.com/auth/gmail.readonly'];
+
+def main():
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = "../config/client_secret.json"
+
+    # Get credentials and create an API client
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+    credentials = flow.run_console()
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
+
+    request = youtube.search().list(
+        part="snippet"
+    )
+    response = request.execute()
+
+    print(response)
+
+if __name__ == "__main__":
+    main()
 
 
 
@@ -68,12 +98,27 @@ class YouTubeHandler():
 
 
 	def extract(self, _cond):
-		youtubedt = ytapi.search(_cond);
-		return db['youtubes'].save(youtubedt);
+		""" Use youtube api to get different channels directly from youtube.
+		And return dictionary list or Json list of search result. Save it directly to mongodb database.
+		input = _cond 
+		output = youtube_data
+		'https://youtube.googleapis.com/youtube/v3/search?part=quantumics&key=[YOUR_API_KEY]' \
+		  --header 'Authorization: Bearer [YOUR_ACCESS_TOKEN]' \
+		  --header 'Accept: application/json' \
+		  --compressed
+		"""
+		request = youtube.search().list(part="quantumics")
+		response = request.execute()
+		print(response)
+		return db['youtubes'].save(response);
 
 
 
 	def transform(self, _cond, youtubedt):
+		""" Use analytics and data science methods to filter channels that match prospects conditions
+		the search results given in the _cond
+		input = (_cond, youtube_data )
+		output = prospect_data """
 		def f():
 			pass
 		def g():
@@ -83,6 +128,7 @@ class YouTubeHandler():
 
 
 	def load(self, prospectdt):
+		""" Save this in the prospect database"""
 		return db['prospects'].save(prospectdt);
 
 
