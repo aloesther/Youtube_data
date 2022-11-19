77a467f58da1858b4c3d491d79b393c5b7d241e6
Shyn Tech <professorbashorun@gmail.com>
2022-11-19 06:12:24 +0100

    updated

--- functions/negotiation.py
+++ functions/negotiation.py
@@ -2,102 +2,101 @@
 import googleapiclient.discovery
 
 # API information
-api_service_name = "youtube"
+api_service_name = "gmail"
 api_version = "v3"
 # API key
 DEVELOPER_KEY = "AIzaSyCYHVvZSGo08mVk5j_Epwf8wuJWDyOOWzk"
 # API client
-youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey = DEVELOPER_KEY)
+gmail = googleapiclient.discovery.build(api_service_name, api_version, developerKey = DEVELOPER_KEY)
 # 'request' variable is the only thing you must change
 # depending on the resource and method you need to use
 # in your query
-request = youtube.search().list()
-# Query execution
-response = request.execute()
-# Print the results
-print(response)
 
 
 
 
-class NegotiationHandler():
+#class NegotiationHandler():
 
 
 
-	def __new__(self):
-		pass
+def __new__(self):
+	pass
 
 
 
-	def __init__(self):
-		pass
+def __init__(self):
+	pass
 
 
 
-	def create(self):
-		pass
+def create(self):
+	pass
 
 
 
-	def save(self, negotiationdt):
-		return db['negotiations'].save(negotiationdt);
+def save(self, negotiationdt):
+	return db['negotiations'].save(negotiationdt);
 
 
 
-	def read(self, _cond):
-		return db['negotiations'].list(_cond);
+def read(self, _cond):
+	return db['negotiations'].list(_cond);
 
 
 
-	def get(self, _cond):
-		return self.read(_cond);
+def get(self, _cond):
+	return self.read(_cond);
 
 
 
-	def list(self, _cond):
-		return self.read(_cond);
+def list(self, _cond):
+	return self.read(_cond);
 
 
 
-	def update(self, _cond, negotiationdt):
-		return db['negotiations'].update(_cond, negotiationdt)
+def update(self, _cond, negotiationdt):
+	return db['negotiations'].update(_cond, negotiationdt)
 
 
 
-	def edit(self, _cond, negotiationdt):
-		return self.update(_cond, negotiationdt);
+def edit(self, _cond, negotiationdt):
+	return self.update(_cond, negotiationdt);
 
 
 
-	def delete(self, _cond):
-		return db['negotiations'].remove(_cond);
+def delete(self, _cond):
+	return db['negotiations'].remove(_cond);
 
 
-	def export(self, _cond):
-		return migrateMongoDB2CSV('negotiations', _cond);
-
-
-
-	def monitor(self, _cond):
-		# This is a monitoring service function/app
-		pass
+def export(self, _cond):
+	return migrateMongoDB2CSV('negotiations', _cond);
 
 
 
-	def generateResponse(self, _cond):
-		gen = pipeline();
-		return gen(_cond);
+def monitor(self, _cond):
+	request = gmail.search();
+	# Query execution
+	response = request.execute()
+	# Print the results
+	print(response)
+	pass
 
 
 
-	def respond(self, _cond, messages):
-		mailer = SMTPMailer()
-		messages = []
-		email_msg = EmailMessage(messages, _cond.frm, _cond.to)
-		messages.append(email_msg)
-		response=mailer.send_messages(*messages)
-		db['negotiations'].update(_cond, messages);
-		return response;
+def generateResponse(self, _cond):
+	gen = pipeline();
+	return gen(_cond);
+
+
+
+def respond(self, _cond, messages):
+	mailer = SMTPMailer()
+	messages = []
+	email_msg = EmailMessage(messages, _cond.frm, _cond.to)
+	messages.append(email_msg)
+	response=mailer.send_messages(*messages)
+	db['negotiations'].update(_cond, messages);
+	return response;
 
 
 
--- functions/utils.py
+++ functions/utils.py
@@ -1,7 +1,7 @@
 import pandas as pd;
 import pymongo as pm;
 from mailshake import SMTPMailer, EmailMessage;
-from transformers import pipeline
+#from transformers import pipeline
 
 
 
@@ -18,14 +18,17 @@ def getDBDIR(name):
 def migrateCSV2MongoDB(name, _cond):
 	df = pd.read_csv(getDBDIR(name));
 	json = df.to_json();
-	db[name].save(json);
+	db[name].insert_many(json);
 	return json;
 
 
 
 def migrateMongoDB2CSV(name, _cond):
-	data=db[name].get({}).to_array();
+	data=db[name].find({}).cursor();
 	csv = pd.read_json(data).to_csv();
+	f = open(getDBDIR(name), "a")
+	f.write(csv);
+	f.close()
 	return csv;
 
 
